05d51f5b7ead36c33127f94f0ea9e3656a3a081d
francisyeremiah <francisyeremiah@gmail.com>
2026-05-05 19:34:35 +0300

    implemented CSS Modules

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,10 @@
+import styles from "./newissue.module.css";
+
 const New = () => {
     return (
         <div>
             <h1>New</h1>
+            <button className={styles.button}>Create Issue</button>
         </div>
     );
 }
